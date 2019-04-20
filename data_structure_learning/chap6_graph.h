43bf1207393bb8a3b680756c24f413007571fcaa
HzKwok <34784284+Guokj-Git@users.noreply.github.com>
2019-04-20 10:12:08 +0800

    first commit

---
+++ data_structure_learning/chap6_graph.h
@@ -0,0 +1,21 @@
+typedef enum {UNDISCOVERED, DISCOVERED, VISITED} VStatus;
+typedef enum {UNDETERMINED, TREE, CROSS, BACKWARD, FORWARD} EType;
+
+template <typename Tv, typename Te> class Graph
+{
+private:
+
+public:
+	Graph();
+	~Graph();
+
+
+};
+
+Graph::Graph()
+{
+}
+
+Graph::~Graph()
+{
+}
